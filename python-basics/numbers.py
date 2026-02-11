0d9bc8d576de18ec391473e30c5be98c6f7516d5
leon.lone <leonmuriithi.kobia@gmail.com>
2026-02-11 10:28:18 +0300

    input

---
+++ python-basics/numbers.py
@@ -0,0 +1,20 @@
+#Name : Leon Kobia
+#Date : 11/02/2026
+
+#program to get add, subtract, divide, multiply to numbers from input
+
+first_number =int( input("Enter the first number"))
+second_number =int( input("Enter the second number"))
+
+
+sum_numbers = first_number + second_number
+difference_numbers = first_number - second_number
+quotient_numbers = first_number / second_number
+product_numbers = first_number * second_number
+
+
+
+print(f"The sum of the numbers : {sum_numbers}")
+print(f"The difference of the numbers : {difference_numbers}")
+print(f"The quotient of the numbers : {quotient_numbers}")
+print(f"The product of the numbers :{product_numbers}")
---
+++ python-basics/user_input.py
@@ -0,0 +1,8 @@
+#Name : Leon Kobia
+#Date : 11/02/2026
+
+#program to get user input
+
+name = input("What is your name?")
+
+print(name)
