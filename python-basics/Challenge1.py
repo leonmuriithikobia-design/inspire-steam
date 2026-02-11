0538a6672da533570d8aec34f608182d02aac94d
leon.lone <leonmuriithi.kobia@gmail.com>
2026-02-11 09:08:15 +0300

    first commit

---
+++ python-basics/Challenge1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade = "A-"
+favorite_course = "Aerospace Engineering"
+cluster_points = 77
+print(f"My favourite university is {university}")
+print(f"I scored{grade} in KCSE")
+print(f"My preferred course is {favorite_course}")
+print(f"My total cluster points were {cluster_points}")
+
---
+++ python-basics/hello.py
@@ -0,0 +1,13 @@
+# This is a comment
+print("Hello from Leon")
+#city
+print("I am from Thika")
+# my hobbies
+print("I like listening to music and watching Tv shows")
+
+age = 18
+height = 1.79
+weight = 84
+print(f"I am {age} years old")
+print(f"I am {height} cm tall")
+print(f"I weigh {weight} Kgs")
