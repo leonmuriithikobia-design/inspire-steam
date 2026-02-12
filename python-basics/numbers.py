605eb5e9b77c316064794fef372d703067ceaa48
leon.lone <leonmuriithi.kobia@gmail.com>
2026-02-12 10:23:12 +0300

    String Formatting

--- python-basics/numbers.py
+++ python-basics/numbers.py
@@ -3,18 +3,28 @@
 
 #program to get add, subtract, divide, multiply to numbers from input
 
-first_number =int( input("Enter the first number"))
-second_number =int( input("Enter the second number"))
+#first_number =int( input("Enter the first number"))
+#second_number =int( input("Enter the second number"))
 
 
-sum_numbers = first_number + second_number
-difference_numbers = first_number - second_number
-quotient_numbers = first_number / second_number
-product_numbers = first_number * second_number
+#sum_numbers = first_number + second_number
+#difference_numbers = first_number - second_number
+#quotient_numbers = first_number / second_number
+#product_numbers = first_number * second_number
 
 
 
-print(f"The sum of the numbers : {sum_numbers}")
-print(f"The difference of the numbers : {difference_numbers}")
-print(f"The quotient of the numbers : {quotient_numbers}")
-print(f"The product of the numbers :{product_numbers}")
+#print(f"The sum of the numbers : {sum_numbers}")
+#print(f"The difference of the numbers : {difference_numbers}")
+#print(f"The quotient of the numbers : {quotient_numbers}")
+#print(f"The product of the numbers :{product_numbers}")
+
+
+third_number = input("What is the third number?")
+
+print(f"The type of third number is: {type(third_number)}")
+
+# Convert third number to an integer
+third_number = int(third_number)
+
+print(f"The type of third number is: {type(third_number)}")
